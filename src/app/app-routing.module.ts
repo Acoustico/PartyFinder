54e3c24e732e0ec4e91ccc346412f7d4d46309d2
Acoustico <migueldiez1@hotmail.com>
2021-05-25 12:24:10 -0700

    muerte prematura seria un milagro

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,15 +1,15 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
-import {SearchpComponent} from './Components/searchp/searchp.component';
-import {ProductsComponent} from './Components/products/products.component';
-import {HomeComponent} from './Components/home/home.component';
-import {CreatepComponent} from "./Components/createp/createp.component";
+import { SearchpComponent } from './Components/searchp/searchp.component';
+import { ProductsComponent } from './Components/products/products.component';
+import { HomeComponent } from './Components/home/home.component';
+import { CreatepComponent } from "./Components/createp/createp.component";
 
 const routes: Routes = [
-  {path: "", component: HomeComponent},
-  {path: "searchp", component: SearchpComponent},
-  {path: "createp", component: CreatepComponent},
-  {path: "products", component: ProductsComponent}
+  { path: "", component: HomeComponent },
+  { path: "searchp", component: SearchpComponent },
+  { path: "createp", component: CreatepComponent },
+  { path: "products", component: ProductsComponent }
 ];
 
 @NgModule({
