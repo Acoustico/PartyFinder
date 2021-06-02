9245eb0873c04504f1abf10cd996036cfdf07b80
Acoustico <migueldiez1@hotmail.com>
2021-06-02 14:04:37 -0700

    hoy

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,12 +3,12 @@ import { RouterModule, Routes } from '@angular/router';
 import { SearchpComponent } from './Components/searchp/searchp.component';
 import { ProductsComponent } from './Components/products/products.component';
 import { HomeComponent } from './Components/home/home.component';
-import { CreatepComponent } from "./Components/createp/createp.component";
+//import { CreatepComponent } from "./Components/createp/createp.component";
 
 const routes: Routes = [
   { path: "", component: HomeComponent },
   { path: "searchp", component: SearchpComponent },
-  { path: "createp", component: CreatepComponent },
+  //{ path: "createp", component: CreatepComponent },
   { path: "products", component: ProductsComponent }
 ];
 
