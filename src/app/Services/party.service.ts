c38f04c4e66d1692730a03337df226aa02918bc7
Acoustico <migueldiez1@hotmail.com>
2021-06-06 14:09:48 -0700

    local-host

--- src/app/Services/party.service.ts
+++ src/app/Services/party.service.ts
@@ -1,8 +1,10 @@
 import { Injectable } from '@angular/core';
 import {HttpClient} from'@angular/common/http';
 import {Party} from '../Model/party'
+import {CrearParty} from '../Model/crearparty';
 import { Observable } from 'rxjs';
 import { catchError, retry } from 'rxjs/operators';
+import { stringify } from '@angular/compiler/src/util';
 
 @Injectable({
     providedIn: 'root'
@@ -10,19 +12,20 @@ import { catchError, retry } from 'rxjs/operators';
 export class PartyService{
     API_URL= "http://localhost:3000/api/party";
     partys: Party[] | undefined;
-    crear_party: Party[] | undefined;
+    crear_party: CrearParty[] | undefined;
     constructor(private http: HttpClient){
-      this.crear_party=[]
+      this.crear_party=[];
     }
       
     getPartys(){
         return this.http.get<Party[]> (this.API_URL);
     }
 
-    addParty(item:Party){
+    addParty(item:CrearParty){
       this.crear_party?.push(item);
-      localStorage.setItem('crear',JSON.stringify(this.crear_party));
-      console.log(this.crear_party);
+      //this.http.post<Party>(this.API_URL, item);
+      localStorage.setItem('Party',JSON.stringify(this.crear_party));
+     // return this.http.post<Party[]>(this.API_URL, JSON.stringify(item));
       
     }
     
