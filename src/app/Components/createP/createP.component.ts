76a15aaa3b2e3a6cdde3a479abbbc4ecf47c0e1a
Alan Rmz <alan_vicente_rmz@hotmail.com>
2021-06-05 18:24:34 -0700

    aa

--- src/app/Components/createP/createP.component.ts
+++ src/app/Components/createP/createP.component.ts
@@ -10,26 +10,16 @@ import { Party } from '../../Model/party';
   styleUrls: ['./createP.component.css']
 })
 export class createPComponent implements OnInit {
-  Party:CParty={}
-  party:Party={ title: new String,
-    language: new String,
-    game: new String,
-    info: new String }
+  
   constructor(public partyService: PartyService) {
-    this.Party=new CParty();
+    
    }
 
   ngOnInit(): void {
-  }
-
-  createparty(form:NgForm){
-    //console.log(form.value);
-    this.partyService.addToMongo(this.party);
-  }
-
-  addParty(party:Party){
-    
-    this.partyService.addToMongo(party);    
-  }
   
+  }
+
+  
+
+ 
 }
